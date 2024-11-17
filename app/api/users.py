740596fc55013974251adbc7a5ec3f8986271aaf
Денис Гришанков <ben300300@gmail.com>
2024-11-17 14:19:47 +0300

    endpoints for change password

--- app/api/users.py
+++ app/api/users.py
@@ -1,10 +1,10 @@
 import typing as tp
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 
-from app.services import users
-from app.schemas.users import (GetUserResponseSchema,
-                               GetUsersUsernameResponseSchema, UserUpdateRequestSchema)
+from app.services import users, email
+from app.schemas.users import (GetUserResponseSchema, GetUsersUsernameResponseSchema,
+                               UserUpdateRequestSchema, EmailSchema, ResetPasswordsSchema)
 
 from app.schemas.tasks import GetUerTasksResponseSchema
 user_routes = APIRouter()
@@ -12,8 +12,8 @@ user_routes = APIRouter()
 
 @user_routes.get("/get_user_tasks/", status_code=201,
                  response_model=GetUerTasksResponseSchema)
-async def get_subscriptions_endpoint(user_id: int):
-    user_tasks = await users.get_all_user_tasks_service(user_id=user_id)
+async def get_subscriptions_endpoint(request: Request):
+    user_tasks = await users.get_all_user_tasks_service(user_id=request.state.user_id)
     return {"tasks": user_tasks}
 
 
@@ -33,24 +33,36 @@ async def get_user_by_username_endpoint(username: str):
 
 @user_routes.get("/get_user_by_email/{email}/", status_code=201,
                  response_model=GetUserResponseSchema)
-async def get_user_by_email_endpoint(email: str) -> tp.Dict[str, tp.Any]:
-    user = await users.get_by_email_service(email=email)
+async def get_user_by_email_endpoint(email_data: str) -> tp.Dict[str, tp.Any]:
+    user = await users.get_by_email_service(email=email_data)
     return user
 
 
-@user_routes.patch("/update_user/{user_id}/",
+@user_routes.patch("/update_user/",
                    response_model=GetUserResponseSchema)
-async def patch_user_endpoint(user_id: int, user_update: UserUpdateRequestSchema):
-    updated_user = await users.update_user_service(user_id, user_update)
+async def patch_user_endpoint(request: Request, user_update: UserUpdateRequestSchema):
+    updated_user = await users.update_user_service(request.state.user_id, user_update)
     if not updated_user:
         raise HTTPException(status_code=404, detail="User not found or update failed")
     return updated_user
 
 
-@user_routes.delete("/delete_user/{user_id}/",
+@user_routes.delete("/delete_user/",
                     status_code=200)
-async def delete_user_endpoint(user_id: int):
-    deleted_user = await users.delete_user_service(user_id=user_id)
+async def delete_user_endpoint(request: Request):
+    deleted_user = await users.delete_user_service(user_id=request.state.user_id)
     if not deleted_user:
         raise HTTPException(status_code=404, detail="User not found")
     return {"detail": "User deleted successfully"}
+
+
+@user_routes.post("/forgot_password/", status_code=200)
+async def forgot_password(email_data: EmailSchema):
+    await email.forgot_password(email=email_data)
+    return {"detail": "Successfully sended email"}
+
+
+@user_routes.post("/reset_password/", status_code=200)
+async def reset_password(data: ResetPasswordsSchema):
+    await email.reset_password_service(data=data)
+    return {"detail": "Successfully changed password"}
